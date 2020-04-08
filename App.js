7d756c1ba194889af3ac5a58119598385a70507f
Konstantin Kirilin <cons.kirilin@gmail.com>
2020-04-08 18:52:26 +0300

    Added alert on item delete

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Alert } from 'react-native';
 import { NavBar } from './src/components/NavBar';
-import { AddTodo } from './src/components/AddTodo';
-import { Todo } from './src/components/Todo';
 import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
   const [todoId, setTodoId] = useState('2')
-  const [todos, setTodos] = useState([{
-    id: '1', title: 'Create Native App',
-    id: '2', title: 'Start Earning More'
-  }])
+  const [todos, setTodos] = useState([
+    {id: '1', title: 'Create Native App'},
+    {id: '2', title: 'Start Earning More'},
+    {id: '3', title: 'Blank'},
+  ])
   
   const addTodo = (title) => {
     setTodos(prev => [
@@ -24,7 +23,27 @@ export default function App() {
   }
 
   const removeTodo = (id) => {
-    setTodos(prev => prev.filter(todo => todo.id !== id))
+    // Works on both Android and iOS
+    const todo = todos.find(todo => todo.id === id)
+    Alert.alert(
+      'Deleting of element',
+      `Are you sure you want to delete ${todo.title}?`,
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        {
+          text: 'Delete',
+          style: 'destructive',
+          onPress: () => {
+            setTodoId(null)
+            setTodos(prev => prev.filter(todo => todo.id !== id))
+          }
+        },
+      ],
+      {cancelable: false},
+    )
   }
 
   const onTodoPress = (id) => {
