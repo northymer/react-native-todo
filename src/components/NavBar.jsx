e667598bc0a84cf16602bf000671fe7656219961
Konstantin Kirilin <konstantin@paulcamper.de>
2020-04-18 16:55:42 +0300

    added style difference between ios and android

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Platform } from 'react-native'
 import { THEME } from '../theme/theme'
 import { AppTextBold } from './AppTextBold'
 
 export const NavBar = ({title}) => {
     return (
-        <View style={styles.navbar}>
+        <View style={{...styles.navbar, ...Platform.select({
+            ios: styles.navbarIos,
+            android: styles.navbarAndroid,
+        })}}>
             <AppTextBold style={styles.text}>{title}</AppTextBold>
         </View>
     )
@@ -15,12 +18,18 @@ const styles = StyleSheet.create({
     navbar: {
         height: 70,
         paddingBottom: 10,
-        backgroundColor: THEME.COLOR_BUSINESS_MAIN,
         justifyContent: 'flex-end',
         alignItems: 'center',
     },
+    navbarAndroid: {
+        backgroundColor: THEME.COLOR_BUSINESS_MAIN,
+    },
+    navbarIos: {
+        borderBottomColor: THEME.COLOR_BUSINESS_MAIN,
+        borderBottomWidth: 1,
+    },
     text: {
         fontSize: 20,
-        color: '#fff',
+        color: Platform.OS === 'ios' ? THEME.COLOR_BUSINESS_MAIN : '#fff',
     }
 })
